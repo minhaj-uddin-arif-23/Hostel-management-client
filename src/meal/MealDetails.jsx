bd345df9c713e78668b6bc914bb9afb713d7c20d
Arif_1 <uarif1933@gmail.com>
2025-01-14 20:49:00 +0600

    Meals details showing complete

--- src/meal/MealDetails.jsx
+++ src/meal/MealDetails.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
-import useMeal from '../Hook/useMeal'
-
+import React from "react";
+import useMeal from "../Hook/useMeal";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "../Hook/useAxiosPublic";
+import { Link, useParams } from "react-router-dom";
+// import { desc } from "framer-motion/m";
+import { SlLike } from "react-icons/sl";
 function MealDetails() {
-  const [meal] = useMeal
+  const { id } = useParams();
+  const axiosPublic = useAxiosPublic();
+  const { data: mealDetails = [] } = useQuery({
+    queryKey: ["mealDetails"],
+    queryFn: async () => {
+      const res = await axiosPublic.get(`/meal/${id}`);
+      return res.data;
+    },
+  });
+  
+  const {
+    title,
+    category,
+    image,
+    price,
+    email,
+    Ingredients,
+    Description,
+    name,
+    like,
+    StartDate,
+    rating,
+    review_count,
+    _id,
+  } = mealDetails || {};
   return (
-    <div>MealDetails</div>
-  )
+    <div>
+      <div className="flex items-center justify-center mt-10">
+        <div className="card card-compact bg-base-100  w-96 shadow-xl ">
+          <figure>
+            <img src={image} className="w-full object-cover" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{title}</h2>
+            <p>{name}</p>
+            <p>{Description}</p>
+            <div className="flex justify-between">
+              <p>{StartDate}</p>
+              <p> Rating {rating}</p>
+            </div>
+            <p className="btn btn-sm">
+              
+               
+               <SlLike /> 
+                {like}
+            </p>
+            <p className="btn btn-sm">Meal Request</p>
+            {/* <p>{}</p> */}
+            <div className="flex justify-between">
+              <p className="font-semibold text-lg">${price}</p>
+              <p p className="font-semibold text-lg ml-28">
+                rating : {rating}
+              </p>
+            </div>
+            <div className="card-actions mt-3">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MealDetails
+export default MealDetails;
